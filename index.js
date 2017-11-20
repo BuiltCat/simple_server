98b94351f501f70133dec1a1ce413861abaeb9d3
小半 <wt920116625@live.com>
2017-11-20 19:12:51 +0800

    辅助中间件修改

--- index.js
+++ index.js
@@ -2,34 +2,47 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const bodyParser = require('body-parser')
+const simplemysql = require('./simple-mysql')
 
 const server = http.createServer(app)
 server.listen(8080)
 
+const info = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'wechart_upload'
+}
+const sql = 'select * from user'
+const mysql = simplemysql(info)
+mysql.middleware(sql)
+
 const users = []
 
-class user{
-    constructor(name, age, course, grade){
+class user {
+    constructor(name, age, course, grade) {
         this.name = name
         this.age = age
         this.course = course
         this.grade = grade
     }
-    static get ['users'](){
+    static get['users']() {
         return users
     }
 }
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.get('/user', (req, res) => {
     res.json(user.users)
 })
 app.post('/user', (req, res) => {
-    user.users.push(new user(req.body.name,req.body.course,req.body.grade))
+    user.users.push(new user(req.body.name, req.body.course, req.body.grade))
     res.json(user.users)
 })
 
-app.use((req,res) => {
+app.use((req, res) => {
     res.end('end')
-}) 
+})
